ab78fc4bb458587fae4600e9b529426386ddd7fe
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-06 03:30:09 +0900

    [Bronze V] Title: ??!, Time: 44 ms, Memory: 5372 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"{input}??!");
+        }
+    }
+}
