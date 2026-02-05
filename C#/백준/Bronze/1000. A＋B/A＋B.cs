4727e0af274d4ac832b0682db50d6e4d2214e462
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-06 02:49:48 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,14 @@
+namespace baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            Console.WriteLine(A+B);
+
+        }
+    }
+}
