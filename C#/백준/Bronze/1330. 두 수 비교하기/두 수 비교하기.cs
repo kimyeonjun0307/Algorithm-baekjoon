7c661a696f9b054d438cfaa15455fcd065dbf1dd
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-06 20:18:13 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 44 ms, Memory: 5480 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -0,0 +1,18 @@
+namespace baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            if (A > B) 
+                Console.WriteLine(">");
+            else if (A < B) 
+                Console.WriteLine("<");
+            else
+                Console.WriteLine("==");
+        }
+    }
+}
