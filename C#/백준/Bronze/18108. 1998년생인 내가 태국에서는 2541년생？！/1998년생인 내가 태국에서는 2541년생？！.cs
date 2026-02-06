7b97c429d6b0bb7406d5dbcbc5fa6e8608528488
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-06 20:12:05 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 44 ms, Memory: 5540 KB -BaekjoonHub

---
+++ C#/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+namespace baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int A = int.Parse(input);
+
+            Console.WriteLine($"{A-543}");
+        }
+    }
+}
