1260e9601cd6f30c55411f239b322dd055302d68
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-14 04:49:35 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A;
+    cin>>A;
+
+    cout<<A*78/100<<" "<<(A*8/10)+((A*2/10)*78/100)<<"\n";
+
+    
+    return 0;
+}
