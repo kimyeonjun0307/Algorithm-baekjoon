c6c872f0a5f745fa4e941bc83cca70ba9f957820
kimyeonjun0307 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-02-14 04:42:48 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A,B;   
+    
+    while(cin>>A>>B)
+    {
+        cout<<A+B<<"\n";
+    }
+    return 0;
+}
