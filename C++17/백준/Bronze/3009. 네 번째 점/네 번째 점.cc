24417967d8f8c8ee2e19046e2b6eccdd048b3489
김연준 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-03-08 22:05:02 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ C++17/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -8,28 +8,7 @@ int main()
 
     int x1,x2,x3,y1,y2,y3;
     cin>>x1>>y1>>x2>>y2>>x3>>y3;
-    if(x1==x2)
-    {
-        cout<<x3;
-    }
-    else if(x2==x3)
-    {
-        cout<<x1;
-    }
-    else if(x1==x3)
-    {
-        cout<<x2;
-    }
-    if(y1==y2)
-    {
-        cout<<" "<<y3;
-    }
-    else if(y2==y3)
-    {
-        cout<<" "<<y1;
-    }
-    else if(y1==y3)
-    {
-        cout<<" "<<y2;
-    }
+    int x= (x1^x2^x3);
+    int y= (y1^y2^y3);
+    cout<<x<<" "<<y;
 }
