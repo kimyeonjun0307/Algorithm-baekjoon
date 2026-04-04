3041abd15d70034365ea5bed9a0068b0d3d57159
김연준 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-04-04 23:00:34 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A,B,C;
+    cin>>A>>B>>C;
+    cout<<(A+B==C?"correct!":"wrong!");
+    
+}
