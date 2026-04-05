fa7f811440081ebca9f54112d0dadde3242c56bd
김연준 <133335763+kimyeonjun0307@users.noreply.github.com>
2026-04-06 00:03:41 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 9832 KB -BaekjoonHub

--- C++17/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ C++17/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long dp[1000001];
+
+long long fib(int n)
+{
+    if (n==0)
+        {return 0;}
+    if (n == 1 || n == 2)
+    {    return 1;}
+    if(dp[n] != 0)
+    {
+        return dp[n];
+    }
+    return dp[n] = fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int A;
-    cin>>A;
-    long long k =0;
-    deque<long long> d;    
-    if(A==1){cout<<1;return 0;}
-    else if(A==2){cout<<1;return 0;}
-    else {
-        d.push_back(1);
-        d.push_back(1);
-        for(int i = 3; i<=A; i++)
-        {
-            k = accumulate(d.begin(),d.end(),0LL);
-            d.pop_front();
-            d.push_back(k);
-        }
-    }
-    cout<<k;
-    
+    cin >> A;
+    cout << fib(A);
 }
